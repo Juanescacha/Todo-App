78758c3af8ed728eb70d90296fee2acdbb00c577
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-11-11 18:43:21 -0500

    feat: :sparkles: todoapp main container

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<script setup>
+	import TodoForm from "./TodoForm.vue"
+	import TodoList from "./TodoList.vue"
+</script>
+
+<template>
+	<main class="todo-app">
+		<h1>To Do List</h1>
+		<TodoForm />
+		<TodoList />
+	</main>
+</template>
+
+<style lang="scss" scoped>
+	.todo-app {
+		padding: 5vh;
+		min-height: 100vh;
+		width: 30vw;
+		background-color: #f7f9fc;
+	}
+</style>
