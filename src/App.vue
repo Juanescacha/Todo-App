6045cd2101c3d2d6a05146d886b68338ff9c0e3b
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-11-11 18:42:49 -0500

    feat: :lipstick: style for main app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script setup>
+	import TodoApp from "./components/TodoApp.vue"
+</script>
 
-<template></template>
+<template>
+	<TodoApp />
+</template>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	#app {
+		background-color: #597d9a;
+		display: flex;
+		justify-content: center;
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+</style>
